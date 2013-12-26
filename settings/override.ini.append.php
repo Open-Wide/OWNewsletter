d110e6aedd333ce89295192a5708487dfd273d9c
Madeline VEYRENC <madeline.veyrenc@openwide.fr>
2013-12-26 10:34:50 +0100

    Remove cjw prefix

--- settings/override.ini.append.php
+++ settings/override.ini.append.php
@@ -1,12 +1,18 @@
 <?php /* #?ini charset="utf-8"? /*
 
-[children.tpl#cjw_newsletter_edition]
+[children.tpl#newsletter_edition]
 Source=children.tpl
 MatchFile=newsletter_mailing_list/children.tpl
 Subdir=templates
 Match[class_identifier]=newsletter_mailing_list
 
-[children_detailed.tpl#cjw_newsletter_edition]
+[children_detailed.tpl#newsletter_edition]
+Source=children_detailed.tpl
+MatchFile=newsletter_mailing_list/children_detailed.tpl
+Subdir=templates
+Match[class_identifier]=newsletter_mailing_list
+
+[children_detailed.tpl#newsletter_edition]
 Source=children_detailed.tpl
 MatchFile=newsletter_mailing_list/children_detailed.tpl
 Subdir=templates
